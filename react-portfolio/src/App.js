6117b62e26c64a8ccef85171e52204865f767d48
Bryan Dumpit <dumpitbr@gmail.com>
2022-11-14 19:12:53 -0500

    initial files and placeholder components

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import About from './components/About';
+import Nav from './components/Nav';
+import Portfolio from './components/Portfolio';
+
+function App() {
+  return (
+    <div>
+      <Nav></Nav>
+      <main>
+        <Portfolio></Portfolio>
+        <About></About>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-portfolio/src/components/Portfolio/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Portfolio(props) {
+    const currentCategory = {
+        name: "commercial",
+        description: "Photos of grocery stores, food trucks, and other commercial projects"
+    }
+    return (
+        <section>
+            <h1>NAME</h1>
+            <p>DESCRIPTION</p>
+        </section>
+    )
+}
+
+export default Portfolio;
