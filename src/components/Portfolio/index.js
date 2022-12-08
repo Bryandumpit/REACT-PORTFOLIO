fe16d83c31e63baa766181a4c8e70e81e025fcb2
Bryan Dumpit <dumpitbr@gmail.com>
2022-12-08 14:02:10 -0500

    added screenshots and clickable previews on portfolio page

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -9,33 +9,40 @@ function Portfolio() {
         {
           name:'RoadTripcierge',
           description:'This project was focused on front-end web development. We built a website where a user can plan their road trip by providing their origin and destination. The website then retrieves information from third-party APIs and provides the user with options for hotels to stay at and a map and directions to get there. ',
-          builtWith:['JavaScript', 'HTML', 'Bulma', 'Google Maps API', 'Hotels.com API', 'Google Geocoding API']
+          builtWith:['JavaScript', 'HTML', 'Bulma', 'Google Maps API', 'Hotels.com API', 'Google Geocoding API'],
+          screenshot: 'roadtripcierge.png',
+          url:'https://bryandumpit.github.io/Road-Tripcierge/'
         },
         {
           name: 'Giglr',
           description: 'Every now and then we need a break from work or study. Sometimes we enjoy scrolling through funny memes and if we have a particularly good one we share them too. Giglr was developed using the MVC model to provide this service to users.',
-          builtWith:['JavaScript', 'Node.js', 'mySQL', 'sequelize', 'bcrypt', 'clooudinary', 'Express.js', 'Bootstrap']
+          builtWith:['JavaScript', 'Node.js', 'mySQL', 'sequelize', 'bcrypt', 'cloudinary', 'Express.js', 'Bootstrap', 'Handlebars.js'],
+          screenshot: 'giglr.png',
+          url:'https://giglr.herokuapp.com/'
         },
         {
-          name: 'Placeholder1',
-          description: 'Lorem Ipsum...',
-          builtWith: ['JavaScript', 'etc.js']
+          name: 'StarShip',
+          description: 'StarShip provides users the platform to track stock market data. Users are able to track the market in general or they can search up a specific stock.',
+          builtWith: ['JavaScript', 'MongoDb', 'Express.js', 'React.js', 'Node.js', 'Apollo', 'GraphQL', 'Alpaca API', 'Material UI'],
+          screenshot: 'roadtripcierge.png',
+          url:'https://still-cliffs-68409.herokuapp.com/'
         },
         {
           name: 'Placeholder2',
           description: 'Lorem Ipsum...',
-          builtWith: ['JavaScript', 'etc.js']
+          builtWith: ['JavaScript', 'etc.js'],
+          screenshot: 'roadtripcierge.png',
+          url:'https://github.com/Bryandumpit'
         }
       ]
     
-      console.log(projects)
-      projects.map(project=>{console.log(project.name, project.description)})
+      
 
     return (
         <section>
             <h1 data-testid="h1tag">Checkout My Works!</h1>
             <h2> Here they are:</h2>
-            <Works projects={projects}></Works>
+            <Works projects={projects}/>
             
             
         </section>
--- src/components/Works/index.js
+++ src/components/Works/index.js
@@ -1,29 +1,30 @@
 import React  from 'react';
 
+
 function Works (props) {
   const {
     projects
   } = props;
 
-  console.log(projects);
-  
-  projects.map(project=>{console.log(project.name)})
-  
   return(
-    <div className="flex-row">
+    <div className="flex-row" >
         {projects.map( (project) => (
           <div className={`mx-1 ${project.name}`} key={project.name}>
             <span> {project.name} </span>
             <div>
+              <a target='_blank' href={`${project.url}`} rel="noreferrer">
+                <img alt={`${project.name}`} src={require(`../../assets/portfolio-images/${project.screenshot}`)}/>
+              </a>
               <p> {project.description}</p>
             </div>
             <ul>
-              {project.builtWith.map(tech=>(
-                <li>
+              {project.builtWith.map((tech,index)=>(
+                <li key={index}>
                   {tech}
                 </li>
               ))}
             </ul>
+            
           </div>
         ))}
     </div>
